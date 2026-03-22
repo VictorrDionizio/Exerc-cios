e1828ad4edf843268f7dada84bb5cb3255b8f627
VictorrDionizio <vicordionizio@gmail.com>
2026-03-22 20:43:26 -0300

    trabalho.c

--- trabalho.c
+++ trabalho.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 int main() {
     int horas, minutos;
-    int totalmin, extra, falta;
+    int totalMin, extra, falta;
     printf("Digite as horas trabalhadas: ");
     scanf("%d", &horas);
     printf("Digite os minutos trabalhados: ");
     scanf("%d", &minutos);
-    totalmin = horas * 60 + minutos;
-    if (totalmin > 480) {
-        extra = totalmin - 480;
+    totalMin = horas * 60 + minutos;
+    if (totalMin > 480) {
+        extra = totalMin - 480;
         printf("Horas adicionadas ao banco: %d horas e %d minutos\n", extra / 60, extra % 60);
-    } else if (totalmin < 480) {
-        falta = 480 - totalmin;
-        printf("Horas retiradas do banco: %d horas e %d minutos\n", falta / 60, falta % 60);
     } else {
-        printf("Nenhuma alteração no banco de horas.\n");
+        if (totalMin < 480) {
+            falta = 480 - totalMin;
+            printf("Horas retiradas do banco: %d horas e %d minutos\n", falta / 60, falta % 60);
+        } else {
+            printf("Nenhuma alteracao no banco de horas.\n");
+        }
     }
 }
