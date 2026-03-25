7d7e5e50580de1856b7e50af7e5f80f76279d120
VictorrDionizio <vicordionizio@gmail.com>
2026-03-25 20:14:07 -0300

    loja.c

--- loja.c
+++ loja.c
@@ -2,7 +2,7 @@
 void main() {
     float totalcompra;
     printf("Digite o total da compra: ");
-    scanf("%f", &totalcompra);\ 
+    scanf("%f", &totalcompra); 
     if( totalcompra > 50.0 ) {
     printf("Parabéns, você ganhou um brinde!");
 }
